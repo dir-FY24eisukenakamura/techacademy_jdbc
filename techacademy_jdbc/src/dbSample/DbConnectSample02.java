88e9aa7b52735b1e4f42e85600e607d2a4eea2f9
eisuke.nakamura.m706@dirbato.co.jp <EisukeNakamura()@Dirbato-1423x>
2024-04-17 11:02:47 +0900

    11時コミット

--- techacademy_jdbc/src/dbSample/DbConnectSample02.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample02.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DbConnectSample2 {
+public class DbConnectSample02 {
 
     public static void main(String[] args) {
         
